2e8b7f421c781cb011c2ac89185a8357a40ab8c7
susmoi <liz.alma.brown@gmail.com>
2023-03-17 09:56:57 -0400

    tweaking

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 // JavaScript
 
-// Get the current URL of the page
-const currentUrl = window.location.href;
+$(document).ready(function(){
 
-// Get all the navigation links
-const navLinks = document.querySelectorAll('.nav-element');
+  // Get the current URL of the page
+  const currentUrl = window.location.href;
 
-// Loop through each link and check if its href attribute matches the current URL
-navLinks.forEach(link => {
-  if (link.href === currentUrl) {
-    link.classList.add('active');
+  // Get all the navigation links
+  const navLinks = document.querySelectorAll('.nav-element');
+
+  // Loop through each link and check if its href attribute matches the current URL
+  navLinks.forEach(link => {
+    if (link.href === currentUrl) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+
+  if (navLinks.length > 0) {
+    console.log(`Successfully found ${navLinks.length} element(s) with class 'nav-element'`);
   } else {
-    link.classList.remove('active');
+    console.log(`No elements found with class 'nav-element'`);
   }
 });
-
-if (navLinks.length > 0) {
-  console.log(`Successfully found ${navLinks.length} element(s) with class 'nav-element'`);
-} else {
-  console.log(`No elements found with class 'nav-element'`);
-}
