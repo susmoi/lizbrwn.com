5bb70a50682570587be51a652316277732c8543a
susmoi <liz.alma.brown@gmail.com>
2023-03-17 10:18:35 -0400

    tweaking

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // JavaScript
 
-$(document).ready(function(){
+//$(document).ready(function(){
 
   // Get the current URL of the page
-  const currentUrl = window.location.href;
+  //const currentUrl = window.location.href;
 
   // Get all the navigation links
-  const navLinks = document.querySelectorAll('.nav-element');
+  //const navLinks = document.querySelectorAll('.nav-element');
 
   // Loop through each link and check if its href attribute matches the current URL
-  navLinks.forEach(link => {
-    if (link.href === currentUrl) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
-  });
+  //navLinks.forEach(link => {
+    //if (link.href === currentUrl) {
+    //  link.classList.add('active');
+    //} else {
+    //  link.classList.remove('active');
+  //  }
+  //});
 
-  if (navLinks.length > 0) {
-    console.log(`Successfully found ${navLinks.length} element(s) with class 'nav-element'`);
-  } else {
-    console.log(`Elements not found with class 'nav-element'`);
-  }
-});
+  //if (navLinks.length > 0) {
+    //console.log(`Successfully found ${navLinks.length} element(s) with class 'nav-//element'`);
+//  } else {
+//    console.log(`Elements not found with class 'nav-element'`);
+//
+//});
