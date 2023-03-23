0f02342a69e12c59aa67eab70e8078358279083b
susmoi <liz.alma.brown@gmail.com>
2023-03-22 23:20:01 -0400

    starting project move resources to JSON

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,73 @@
 // JavaScript
+// import files/resources JSON files
 
-//$(document).ready(function(){
+$(document).ready(function(){
 
-  // Get the current URL of the page
-  //const currentUrl = window.location.href;
+  // Make sure the document is loaded
+  // for each JSON file, create html
 
-  // Get all the navigation links
-  //const navLinks = document.querySelectorAll('.nav-element');
 
-  // Loop through each link and check if its href attribute matches the current URL
-  //navLinks.forEach(link => {
-    //if (link.href === currentUrl) {
-    //  link.classList.add('active');
-    //} else {
-    //  link.classList.remove('active');
-  //  }
-  //});
 
-  //if (navLinks.length > 0) {
-    //console.log(`Successfully found ${navLinks.length} element(s) with class 'nav-//element'`);
-//  } else {
-//    console.log(`Elements not found with class 'nav-element'`);
-//
-//});
+});
+
+function jsonToHtml(jsonDict) {
+  const resourceLink = jsonDict["Resource link"];
+  const resourceName = jsonDict["Resource name"];
+  const resourceDescription = jsonDict["Resource description"];
+  const resourceTag = jsonDict["Resource tag"];
+
+  const html = `
+                <a href="${resourceLink}">
+                  <div class="resource-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h2 class="resource-name mb-1">${resourceName}</h2>
+                    </div>
+                    <p class="resource-text mb-1">${resourceDescription}</p>
+                    <small class="text-muted">${resourceTag}</small>
+                  </div>
+                </a>
+                  `;
+                  return html;
+};
+
+// Function to fetch JSON files from files/resources
+async function fetchJsonFiles() {
+  try {
+    // List of JSON filenames to import
+    const jsonFilenames = ['tools.json', 'guides.json', 'content.json', 'statistics',];
+    // creates an array of promises based on the jasonFilenames array.
+    //
+    const jsonFiles = await Promise.all(jsonFilenames.map(async (filename) => {
+      const response = await fetch(`files/resources/${filename}`);
+      if (response.ok) {
+        return await response.json();
+      } else {
+        throw new Error(`Error fetching ${filename}: ${response.statusText}`);
+      }
+    }));
+
+    return jsonFiles;
+
+  } catch (error) {
+    console.error('Error fetching JSON files:', error);
+    return [];
+  }
+}
+
+// Function to process JSON dictionaries
+async function processJsonFiles() {
+  const jsonFiles = await fetchJsonFiles();
+
+  // Assuming you have a container div with id 'resources-container'
+  const resourcesContainer = document.getElementById('resources-container');
+
+  for (const jsonFile of jsonFiles) {
+    for (const jsonDict of jsonFile) {
+      const html = jsonToHtml(jsonDict);
+      resourcesContainer.innerHTML += html;
+    }
+  }
+}
+
+// Call the processJsonFiles function
+processJsonFiles();
