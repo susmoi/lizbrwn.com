56ed0970a50bab3edbf456c1dc17203b3dd76a37
susmoi <liz.alma.brown@gmail.com>
2023-03-30 22:33:18 -0400

    testing a new resource page, still

--- scripts/resources-script.js
+++ scripts/resources-script.js
@@ -1,29 +1,34 @@
 // JavaScript
 // import files/resources JSON files
 
-$(document).ready(function(){
+$(document).ready(function() {
+  console.log("Document ready");
 
-  // Make sure the document is loaded
-  // for each JSON file, create html
+  $.getJSON("files/resources/content.json", function(data) {
+    console.log("JSON retrieved successfully");
 
+    var resourcesHtml = "";
 
+    $.each(data, function(key, val) {
+      resourcesHtml += "<div class='resource'>";
+      resourcesHtml += "<a href='" + val['Resource link'] + "'>" + val['Resource name'] + "</a>";
+      resourcesHtml += "<p>" + val['Resource description'] + "</p>";
+      resourcesHtml += "<span>" + val['Resource tag'] + "</span>";
 
+      if (val['Resource placement']) {
+        resourcesHtml += "<span>" + val['Resource placement'] + "</span>";
+      }
 
+      resourcesHtml += "</div>";
+    });
 
-$.getJSON("files/resources/content.json", function(data) {
-  var resourcesHtml = "";
+    console.log("HTML generated");
 
-  $.each(data, function(key, val) {
-    resourcesHtml += "<div class='resource'>";
-    resourcesHtml += "<a href='" + val['Resource link'] + "'>" + val['Resource name'] + "</a>";
-    resourcesHtml += "<p>" + val['Resource description'] + "</p>";
-    resourcesHtml += "<span>" + val['Resource tag'] + "</span>";
-    if (resource['Resource placement']) {
-      resourcesHtml += "<span>" + resource['Resource placement'] + "</span>";
-    }
-    resourcesHtml += "</div>";
+    $("#resources-test").html(resourcesHtml);
+    console.log("HTML inserted into page");
+  }).fail(function() {
+    console.log("Failed to retrieve JSON data");
   });
 
-  $("#resources-test").html(resourcesHtml);
-});
+  console.log("End of document ready function");
 });
