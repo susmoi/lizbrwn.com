c7b930f389d1a34d441f9bf332d4b858f48a17fb
susmoi <liz.alma.brown@gmail.com>
2023-03-30 22:56:02 -0400

    it works! adding more files to see if it breaks

--- scripts/resources-script.js
+++ scripts/resources-script.js
@@ -4,6 +4,7 @@
 $(document).ready(function() {
   console.log("Document ready");
 
+//GET CONTENT
   $.getJSON("files/resources/content.json", function(data) {
     console.log("JSON retrieved successfully");
 
@@ -29,6 +30,41 @@ $(document).ready(function() {
   }).fail(function() {
     console.log("Failed to retrieve JSON data");
   });
+
+//GET TOOLS
+  $.getJSON("files/resources/tools.json", function(data) {
+    console.log("JSON retrieved successfully");
+
+    var resourcesHtml = "";
+
+    $.each(data, function(key, val) {
+      resourcesHtml += "<div class='resource'>";
+      resourcesHtml += "<a href='" + val['Resource link'] + "'>" + val['Resource name'] + "</a>";
+      resourcesHtml += "<p>" + val['Resource description'] + "</p>";
+      resourcesHtml += "<span>" + val['Resource tag'] + "</span>";
+
+      if (val['Resource placement']) {
+        resourcesHtml += "<span>" + val['Resource placement'] + "</span>";
+      }
+
+      resourcesHtml += "</div>";
+    });
+
+    console.log("HTML generated");
+
+    $("#resources").html(resourcesHtml);
+    console.log("HTML inserted into page");
+  }).fail(function() {
+    console.log("Failed to retrieve JSON data");
+  });
+
+
+
+
+
+
+
+
 
   console.log("End of document ready function");
 });
