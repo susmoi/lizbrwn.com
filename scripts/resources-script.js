7a9aef1a9b0ff7fa548ae7de544ff77de19cde10
susmoi <liz.alma.brown@gmail.com>
2023-03-30 22:16:40 -0400

    testing a new resource page

---
+++ scripts/resources-script.js
@@ -0,0 +1,26 @@
+// JavaScript
+// import files/resources JSON files
+
+$(document).ready(function(){
+
+  // Make sure the document is loaded
+  // for each JSON file, create html
+
+
+
+
+
+$.getJSON("files/resources/content.json", function(data) {
+  var resourcesHtml = "";
+
+  $.each(data, function(key, val) {
+    resourcesHtml += "<div class='resource'>";
+    resourcesHtml += "<a href='" + val['Resource link'] + "'>" + val['Resource name'] + "</a>";
+    resourcesHtml += "<p>" + val['Resource description'] + "</p>";
+    resourcesHtml += "<span>" + val['Resource tag'] + "</span>";
+    resourcesHtml += "</div>";
+  });
+
+  $("#resources-test").html(resourcesHtml);
+});
+});
