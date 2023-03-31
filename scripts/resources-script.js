ee41cc61cb9e6970a55a99cc78cb249ae8916edc
susmoi <liz.alma.brown@gmail.com>
2023-03-31 10:26:00 -0400

    adding all the resources to the dynamic page, hope it does not break it!

--- scripts/resources-script.js
+++ scripts/resources-script.js
@@ -4,14 +4,16 @@
 $(document).ready(function() {
   console.log("Document ready");
 
-//GET CONTENT
-  $.getJSON("files/resources/content.json", function(data) {
+
+
+//GET TOOLS
+  $.getJSON("files/resources/tools.json", function(data) {
     console.log("JSON retrieved successfully");
 
     var resourcesHtml = "";
 
     //Iterate through the file and input the values as HTML elements
-  $.each(data, function(key, val) {
+    $.each(data, function(key, val) {
       resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
       resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
       resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
@@ -23,38 +25,95 @@ $(document).ready(function() {
       resourcesHtml += "</a>";
     });
     console.log("HTML generated");
-      //input the HTML in the element with the #content-resources ID
-    $("#content-resources").html(resourcesHtml);
-    console.log("HTML inserted into page");
-  }).fail(function() {
-    console.log("Failed to retrieve JSON data");
-  });
-
-//GET TOOLS
-  $.getJSON("files/resources/tools.json", function(data) {
-    console.log("JSON retrieved successfully");
-
-    var resourcesHtml = "";
-
-    //Iterate through the file and input the values as HTML elements
-    $.each(data, function(key, val) {
-      resourcesHtml += "<div class='resource'>";
-      resourcesHtml += "<a href='" + val['Resource link'] + "'>" + val['Resource name'] + "</a>";
-      resourcesHtml += "<p>" + val['Resource description'] + "</p>";
-      resourcesHtml += "<span>" + val['Resource tag'] + "</span>";
-
-      if (val['Resource placement']) {
-        resourcesHtml += "<span>" + val['Resource placement'] + "</span>";
-      }
-      resourcesHtml += "</div>";
-    });
-    console.log("HTML generated");
     //input the HTML in the element with the #tools-resources ID
     $("#tools-resources").html(resourcesHtml);
     console.log("HTML inserted into page");
   }).fail(function() {
     console.log("Failed to retrieve JSON data");
   });
+
+
+  //GET GUIDES
+    $.getJSON("files/resources/guides.json", function(data) {
+      console.log("JSON retrieved successfully");
+
+      var resourcesHtml = "";
+
+      //Iterate through the file and input the values as HTML elements
+    $.each(data, function(key, val) {
+        resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
+        resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
+        resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
+        resourcesHtml += "<h5 class='mb-1' style='color:#FF6600'>" + val['Resource name'] + "</h5>";
+        resourcesHtml += "</div>";
+        resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
+        resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
+        resourcesHtml += "</div>";
+        resourcesHtml += "</a>";
+      });
+      console.log("HTML generated");
+        //input the HTML in the element with the #guides-resources ID
+      $("#content-resources").html(resourcesHtml);
+      console.log("HTML inserted into page");
+    }).fail(function() {
+      console.log("Failed to retrieve JSON data");
+    });
+
+
+
+
+  //GET CONTENT
+    $.getJSON("files/resources/content.json", function(data) {
+      console.log("JSON retrieved successfully");
+
+      var resourcesHtml = "";
+
+      //Iterate through the file and input the values as HTML elements
+    $.each(data, function(key, val) {
+        resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
+        resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
+        resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
+        resourcesHtml += "<h5 class='mb-1' style='color:#FF6600'>" + val['Resource name'] + "</h5>";
+        resourcesHtml += "</div>";
+        resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
+        resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
+        resourcesHtml += "</div>";
+        resourcesHtml += "</a>";
+      });
+      console.log("HTML generated");
+        //input the HTML in the element with the #content-resources ID
+      $("#content-resources").html(resourcesHtml);
+      console.log("HTML inserted into page");
+    }).fail(function() {
+      console.log("Failed to retrieve JSON data");
+    });
+
+    //GET STATISTICS
+      $.getJSON("files/resources/guides.json", function(data) {
+        console.log("JSON retrieved successfully");
+
+        var resourcesHtml = "";
+
+        //Iterate through the file and input the values as HTML elements
+      $.each(data, function(key, val) {
+          resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
+          resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
+          resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
+          resourcesHtml += "<h5 class='mb-1' style='color:#FF6600'>" + val['Resource name'] + "</h5>";
+          resourcesHtml += "</div>";
+          resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
+          resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
+          resourcesHtml += "</div>";
+          resourcesHtml += "</a>";
+        });
+        console.log("HTML generated");
+          //input the HTML in the element with the #guides-resources ID
+        $("#statistics-resources").html(resourcesHtml);
+        console.log("HTML inserted into page");
+      }).fail(function() {
+        console.log("Failed to retrieve JSON data");
+      });
+
 
 
 
