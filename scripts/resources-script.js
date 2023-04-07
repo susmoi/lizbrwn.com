bf3857f942b61ad90282320ad06993f8b1f39fbf
susmoi <liz.alma.brown@gmail.com>
2023-04-07 19:39:44 -0400

    increase font size

--- scripts/resources-script.js
+++ scripts/resources-script.js
@@ -4,8 +4,6 @@
 $(document).ready(function() {
   console.log("Document ready");
 
-
-
 //GET TOOLS
   $.getJSON("files/resources/alpha.json", function(data) {
     console.log("ALPHA.JSON retrieved successfully");
@@ -29,93 +27,8 @@ $(document).ready(function() {
     $("#az-resources").html(resourcesHtml);
     console.log("Alpha. JSON HTML inserted into page");
     }).fail(function() {
-      console.log("Failed to retrieve ALPHA JSON data");
+      console.log("Failed to retrieve ALPHA JSON data. Validate JSON.");
     });
-
-
-  //GET GUIDES
-//    $.getJSON("files/resources/guides.json", function(data) {
-//       console.log("JSON retrieved successfully");
-
-//       var resourcesHtml = "";
-
-      //Iterate through the file and input the values as HTML elements
-//     $.each(data, function(key, val) {
-//         resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
-//         resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
-//         resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
-//         resourcesHtml += "<h2 class='resource-name mb-1'>" + val['Resource name'] + "</h2>";
-//         resourcesHtml += "</div>";
-//         resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
-//         resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
-//         resourcesHtml += "</div>";
-//         resourcesHtml += "</a>";
-//       });
-//       console.log("HTML generated");
-        //input the HTML in the element with the #guides-resources ID
-//       $("#guides-resources").html(resourcesHtml);
-//       console.log("HTML inserted into page");
-//     }).fail(function() {
-//       console.log("Failed to retrieve JSON data");
-//     });
-
-
-
-
-  //GET CONTENT
-//     $.getJSON("files/resources/content.json", function(data) {
-//       console.log("JSON retrieved successfully");
-
-//       var resourcesHtml = "";
-
-      //Iterate through the file and input the values as HTML elements
-//     $.each(data, function(key, val) {
-//         resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
-//         resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
-//         resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
-//         resourcesHtml += "<h2 class='resource-name mb-1'>" + val['Resource name'] + "</h2>";
-//         resourcesHtml += "</div>";
-//         resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
-//         resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
-//         resourcesHtml += "</div>";
-//         resourcesHtml += "</a>";
-//       });
-//       console.log("HTML generated");
-        //input the HTML in the element with the #content-resources ID
-//       $("#content-resources").html(resourcesHtml);
-//       console.log("HTML inserted into page");
-//     }).fail(function() {
-//       console.log("Failed to retrieve JSON data");
-//     });
-
-    //GET STATISTICS
-//       $.getJSON("files/resources/stats.json", function(data) {
-//         console.log("JSON retrieved successfully");
-
-//         var resourcesHtml = "";
-
-        //Iterate through the file and input the values as HTML elements
-//       $.each(data, function(key, val) {
-//           resourcesHtml += "<a href='" + val['Resource link'] + "' class='resource-link' target='_blank'>";
-//           resourcesHtml += "<div class='resource-item most-frequent list-group-item-action'>";
-//           resourcesHtml += "<div class='d-flex w-100 justify-content-between'>";
-//           resourcesHtml += "<h2 class='resource-name mb-1'>" + val['Resource name'] + "</h2>";
-//           resourcesHtml += "</div>";
-//           resourcesHtml += "<p class='resource-text mb-1'>" + val['Resource description'] + "</p>";
-//           resourcesHtml += "<small class='text-muted'>" + val['Resource tag'] + "</small>";
-//           resourcesHtml += "</div>";
-//           resourcesHtml += "</a>";
-//         });
-//         console.log("HTML generated");
-          //input the HTML in the element with the #guides-resources ID
-//         $("#statistics-resources").html(resourcesHtml);
-//         console.log("HTML inserted into page");
-//       }).fail(function() {
-//         console.log("Failed to retrieve STATS JSON data");
-//      });
-
-
-
 
 // END of functions
   console.log("End of document ready function");
